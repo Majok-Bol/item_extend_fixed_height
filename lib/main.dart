7e6b6f35432dbf99b3b8195f7c608c96665bf9b5
Majok-Bol <bolmajok001@gmail.com>
2025-04-02 21:44:46 +0300

    added images to the home page

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,19 @@ void main(){
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget{
+  final List<String>imagesPath=[
+    "assets/images/toy1.webp",
+        "assets/images/toy2.jpeg",
+        "assets/images/toy3.webp",
+        "assets/images/toy4.webp",
+        "assets/images/toy5.jpeg",
+        "assets/images/toy6.webp",
+        "assets/images/toy7.jpg",
+        "assets/images/toy8.webp",
+        "assets/images/toy9.jpeg",
+        "assets/images/toy10.webp",
+
+  ];
   @override
   Widget build(BuildContext context){
     return MaterialApp(
@@ -10,6 +23,17 @@ class MyApp extends StatelessWidget{
       home: Scaffold(
         backgroundColor: Colors.grey[400],
         appBar: AppBar(title: Text('List extent',style: TextStyle(color: Colors.white,fontSize: 25),),backgroundColor: Colors.purple,centerTitle: true,),
+        body: ListView.builder(
+          itemExtent: 300,
+          itemCount: imagesPath.length,
+            itemBuilder: (context,index){
+            return ListTile(
+              contentPadding: EdgeInsets.all(10),
+              title:Expanded(child: Image.asset(imagesPath[index])),
+
+            );
+
+        }),
       ),
     );
   }
